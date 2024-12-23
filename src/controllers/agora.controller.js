faa48b9354ab5d94dbfe77522e8db23d8f639160
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-12-23 11:21:46 +0500

    code clean

--- src/controllers/agora.controller.js
+++ src/controllers/agora.controller.js
@@ -5,7 +5,7 @@ import statusCodes from "http-status-codes"
 export const generateAgoraToken = async (req, res) => {
     const { channelName, uid } = req.body;
     const token = generateAgoraTokenHelper(channelName, uid)
-    console.log("token---------", token);
-    let response = new HTTPResponse("Token Generated successfully", statusCodes.CREATED)
+    // console.log("token---------", token);
+    let response = new HTTPResponse("Token Generated successfully", token)
     return res.status(statusCodes.CREATED).json(response)
 }
